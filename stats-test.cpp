9094696d3783e909bb32906c69a8b8ade83a2324
Shwetha Shetty K <104064727+ShwethaShettyK@users.noreply.github.com>
2024-08-29 18:29:54 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -1,21 +1,29 @@
-#include "stats.h"
+#include "pch.h"
 
-#include "gtest/gtest.h"
+//#include "gtest/gtest.h"
+#include "stats.h"
 #include <cmath>
+using namespace Statistics;
+//TEST(TestCaseName, TestName) {
+//  EXPECT_EQ(1, 1);
+//  EXPECT_TRUE(true);
+//}
 
 TEST(Statistics, ReportsAverageMinMax) {
-    auto computedStats = Statistics::ComputeStatistics({1.5, 8.9, 3.2, 4.5});
-    float epsilon = 0.001;
-    EXPECT_LT(std::abs(computedStats.average - 4.525), epsilon);
-    EXPECT_LT(std::abs(computedStats.max - 8.9), epsilon);
-    EXPECT_LT(std::abs(computedStats.min - 1.5), epsilon);
+    Stats s = ComputeStatistics({ 1.5, 8.9, 3.2, 4.5 });
+    double epsilon = 0.001;
+    EXPECT_LT(std::abs(s.average - 4.525), epsilon);
+    EXPECT_LT(std::abs(s.max - 8.9), epsilon);
+    EXPECT_LT(std::abs(s.min - 1.5), epsilon);
 }
 
 TEST(Statistics, AverageNaNForEmpty) {
-    auto computedStats = Statistics::ComputeStatistics({});
-    //All fields of computedStats (average, max, min) must be
-    //NAN (not-a-number), as defined in math.h
-    
-    //Design the gtest EXPECT statement here.
-    //Use http://www.cplusplus.com/reference/cmath/isnan/
+    Stats s = ComputeStatistics({ NAN,(INFINITY - INFINITY),HUGE_VAL * 0.0F,NAN });
+
+    EXPECT_TRUE(std::isnan(s.average));
+    EXPECT_TRUE(std::isnan(s.min));
+    EXPECT_TRUE(isnan(s.max));
+
 }
+
+
